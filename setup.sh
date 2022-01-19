9c5ca24a7eb1ac22c2e1d150764037af4e92749e
Michał Krowiarz <m.krowiarz@gmail.com>
2022-01-20 00:13:58 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,29 +1,39 @@
 #!/bin/bash
 
-
-DOCKER_COMPOSE_VERSION=${DOCKER_COMPOSE_VERSION:=1.29.2}
+DOCKER_COMPOSE_VERSION=${DOCKER_COMPOSE_VERSION:=v2.2.3}
+SYSTEM=$(uname -s)
+ARCHITECTURE=$(uname -m)
+HOMEDIR=$(sudo -u $USER sh -c 'echo $HOME')
 
 # Install apt packages
 sudo apt update
 sudo apt upgrade
 sudo apt install -y python3-pip zsh htop git curl tldr
 
+# Make snap work under WSL2
+sudo apt-get update && sudo apt-get install -yqq daemonize dbus-user-session fontconfig && \
+sudo daemonize /usr/bin/unshare --fork --pid --mount-proc /lib/systemd/systemd --system-unit=basic.target exec sudo nsenter -t $(pidof systemd) -a su - $LOGNAME
+
+sudo snap install phpstorm --classic
+
 # Set ZSH as default shell
 sudo usermod -s /usr/bin/zsh $(whoami)
 
 # Install Antigen (ZSH plugin manager)
-curl -L git.io/antigen > ~/.antigen.zsh
+curl -L git.io/antigen > ${HOMEDIR}/.antigen.zsh
 
 # Copy configuration scripts
-cp .zshrc ~/.zshrc
-cp .antigenrc ~/.antigenrc
-cp .p10k.zsh ~/.p10k.zsh
+cp .zshrc ${HOMEDIR}/.zshrc
+cp .antigenrc ${HOMEDIR}./.antigenrc
+cp .p10k.zsh ${HOMEDIR}/.p10k.zsh
+
+antigen theme spaceship-prompt/spaceship-prompt
 
 # Install Docker
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 
-sudo curl -L "https://github.com/docker/compose/releases/download/v${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-${SYSTEM,,}-${ARCHITECTURE,,}" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 sudo groupadd docker
