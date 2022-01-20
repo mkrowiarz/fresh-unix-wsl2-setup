794d1ad0a29776a7570ca1337dcdf08c687fb0bf
Michał Krowiarz <m.krowiarz@gmail.com>
2022-01-20 01:46:45 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -14,8 +14,6 @@ sudo apt install -y python3-pip zsh htop git curl tldr
 git clone https://github.com/DamionGans/ubuntu-wsl2-systemd-script.git $HOMEDIR/ubuntu-wsl2-systemd-script
 $HOMEDIR/ubuntu-wsl2-systemd-script/ubuntu-wsl2-systemd-script.sh --force
 
-sudo snap install phpstorm --classic
-
 # Set ZSH as default shell
 sudo usermod -s /usr/bin/zsh $(whoami)
 
@@ -44,17 +42,3 @@ sudo usermod -aG docker $SUDO_USER
 
 # Allow to run docker daemon without sudo
 sudo echo "$SUDO_USER ALL=(ALL) NOPASSWD: /usr/bin/dockerd" | (sudo su -c 'EDITOR="tee" visudo -f /etc/sudoers.d/docker')
-#
-# DOCKER_DIR=/mnt/wsl/shared-docker
-# mkdir -pm o=,ug=rwx "$DOCKER_DIR"
-# chgrp docker "$DOCKER_DIR"
-#
-# sudo mkdir -p /etc/docker/
-# cp resources/etc/docker/daemon.json /etc/docker/daemon.json
-
-echo '# Start Docker daemon automatically when logging in if not running.' >> ${HOMEDIR}/.zshrc
-echo 'RUNNING=`ps aux | grep dockerd | grep -v grep`' >> ${HOMEDIR}/.zshrc
-echo 'if [ -z "$RUNNING" ]; then' >> ${HOMEDIR}/.zshrc
-echo '    sudo dockerd > /dev/null 2>&1 &' >> ${HOMEDIR}/.zshrc
-echo '    disown' >> ${HOMEDIR}/.zshrc
-echo 'fi' >> ${HOMEDIR}/.zshrc
